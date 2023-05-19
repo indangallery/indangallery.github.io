b661671a17f0fe72b035b3e04c77ad7f25edef59
danufirda <thedanufirda@gmail.com>
2023-05-19 23:13:33 +0700

    update

--- custom.js
+++ custom.js
@@ -1,4 +1,9 @@
-const url = window.location;
+let url = window.location;
+let url_s = url.href.search("/#")
+if(url_s != '-1'){
+    let url_is = url.href.split('/#')
+    url = url_is[0];
+}
 
 const project_html = (dat)=>{
     return `<div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -13,12 +18,49 @@ const project_html = (dat)=>{
 }
 
 $.ajax({
-    url: url+'/project.json',
+    url: url+'/json/project.json',
     cache: false,
     success: function(resp){
-        console.log(resp);
         resp.forEach(dat => {
             $("#project-item").append(project_html(dat));
+        });
+    }
+});
+
+const finishing_html = (dat)=>{
+    return `<div class="col-lg-4 col-md-6  first">
+    <div class="rounded overflow-hidden">
+        <img class="img-fluid" src="karikatur-assets/foto/${dat.image}" alt="">
+    </div>
+</div>`;
+}
+
+$.ajax({
+    url: url+'/json/finishing.json',
+    cache: false,
+    success: function(resp){
+        resp.forEach(dat => {
+            $("#finishing-item").append(finishing_html(dat));
+        });
+    }
+});
+
+const testi_html = (dat)=>{
+    return `<div class="testimonial-item text-center">
+    <div class="col-lg-4 col-md-6 portfolio-item first">
+        <div class="portfolio-img rounded overflow-hidden">
+            <img class="img-fluid" src="karikatur-assets/foto/${dat.image}" alt="">
+        </div>
+    </div>
+</div>`;
+}
+
+$.ajax({
+    url: url+'/json/testimoni.json',
+    cache: false,
+    success: function(resp){
+        resp.forEach(dat => {
+            $("#testi-item").append(testi_html(dat));
         });
     }
 });
